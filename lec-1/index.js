3dd3ba89e35431b38e19c320ae9b2567df234696
Pawan kumar <kumarpy5890@gmail.com>
2026-07-16 10:37:45 +0530

    Initial commit

---
+++ lec-1/index.js
@@ -0,0 +1,21 @@
+var name; //variable declaration 
+name = "Pawan" //value initialization
+
+let age=25;
+
+const country = "india";
+
+console.log(name);
+var name;
+name = "hello world"
+
+console.log(name);
+age = 26;
+console.log(age);
+
+const country = "USA";
+console.log(country);
+
+const age;
+age=10;
+console.log(age);
