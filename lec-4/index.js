576d6e2d2bd18583b68a8c7a0a6b964bf00f07d7
Pawan kumar <kumarpy5890@gmail.com>
2026-07-21 16:56:19 +0530

    lec4

---
+++ lec-4/index.js
@@ -0,0 +1,23 @@
+var a;
+let b;
+console.log(a);
+console.log(b);
+//console.log(c);
+// function fun(){
+//     var c=10;
+// }
+// fun();
+// {
+//     var c=20;
+// }
+// console.log(c);
+var d=null;
+
+function Sum(a,b){
+    console.log(typeof b);
+    return a+b;
+}
+console.log(Sum(10));
+
+console.log(typeof null);
+
