86fff90ab24761a3e401ca5dcff7b6633f8980ae
Pawan kumar <kumarpy5890@gmail.com>
2026-07-22 20:14:34 +0530

    lec-5

---
+++ lec-5/index.js
@@ -0,0 +1,14 @@
+// console.log(a);
+// var a = 10;
+// console.log(a);
+// fun();
+
+// function fun(){
+//     console.log("Hello world");
+// }
+var a=10;
+function fun(){
+    console.log(a);
+    var a=20;
+}
+fun();
