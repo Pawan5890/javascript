e412b9de4c9933659b169b5871752411dfae17eb
Pawan kumar <kumarpy5890@gmail.com>
2026-09-17 09:39:29 +0530

    Add async timing examples
    Expanded the asynchronous programming lesson with setTimeout and setInterval examples, including clearing the interval after a delay. Also added a short note explaining JavaScript's single-threaded, synchronous-by-default behavior.

--- lec-14_Asynchronous_programming/indec.js
+++ lec-14_Asynchronous_programming/indec.js
@@ -1,22 +1,37 @@
 // set time out :- it a function that is used to delay the execution of a function by 
 // a specified amount of time. 
 
-console.log("start");
-setTimeout(function(){
-    console.log("work to do");
+// console.log("start");
+// setTimeout(function(){
+//     console.log("work to do");
+// }, 1000);
+
+// setTimeout(function(){
+//     console.log("timeout 2");
+// }, 500);
+// console.log("end");
+
+// setTimeout(function(){
+//     console.log("timeout 3");
+// }, 200);
+// console.log("hi");
+// setTimeout(function(){
+//     console.log("timeout 4");
+// }, 1500);
+
+// console.log("end");
+
+// set interval :- it is a function that is used to execute a function repeatedly 
+// at a specified interval of time.
+let id3 = setInterval(function(){
+    console.log("Hello");
 }, 1000);
 
 setTimeout(function(){
-    console.log("timeout 2");
-}, 500);
-console.log("end");
+    clearInterval(id3);
+}, 5000);
 
-setTimeout(function(){
-    console.log("timeout 3");
-}, 200);
-console.log("hi");
-setTimeout(function(){
-    console.log("timeout 4");
-}, 1500);
+// different between asynchronous and synchronous programming :-
+// Note :- java script is a single threaded programming language. it means that it can only execute one task at a time.
+// so that js is a synchronous programming language in nature.it is blocking in nature. 
 
-console.log("end");
